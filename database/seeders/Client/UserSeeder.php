559f32826f30b07e0168d2798ff69877379dd1ad
Abdulrahman_Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-03-27 12:20:56 +0400

    Write Database Seeder Structure

---
+++ database/seeders/Client/UserSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders\Client;
+
+use Domain\Client\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::query()->firstOrCreate([
+            'email' => 'admin@project.com',
+        ], [
+            'name' => 'Admin',
+            'email_verified_at' => now(),
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
+    }
+}
