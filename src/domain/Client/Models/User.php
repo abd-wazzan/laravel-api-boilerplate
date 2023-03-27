bcb731a2c82461f9220c99076d1bfc8a0cfe9de0
Abdulrahman_Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-03-27 15:56:24 +0400

    Replace Sanctum With Passport For Authentication.

--- src/domain/Client/Models/User.php
+++ src/domain/Client/Models/User.php
@@ -7,7 +7,7 @@ use Database\Factories\Client\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
