f0809a9c542ccc9b075c59dd572b0619143525b2
Abdulrahman_Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-03-27 13:09:50 +0400

    Rename ForceJsonResponse Middleware to SetAcceptJsonHeader

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -48,7 +48,7 @@ class Kernel extends HttpKernel
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'json.response'
+            \App\Http\Middleware\SetAcceptJsonHeader::class
         ],
     ];
 
@@ -70,6 +70,5 @@ class Kernel extends HttpKernel
         'signed' => \App\Http\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'json.response' => \App\Http\Middleware\ForceJsonResponse::class,
     ];
 }
--- src/app/Http/Middleware/SetAcceptJsonHeader.php
+++ src/app/Http/Middleware/SetAcceptJsonHeader.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class ForceJsonResponse
+class SetAcceptJsonHeader
 {
     public function handle(Request $request, Closure $next)
     {
