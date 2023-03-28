70741a2c39d6c7874c4cfb29e6dabfd07ad9fecb
Abdulrahman_Wazzan <abdulrahmanwazzan.pro@gmail.com>
2023-03-28 21:52:47 +0400

    Enable Model Strict Mode In None Production Environments

--- src/app/Providers/AppServiceProvider.php
+++ src/app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\Relation;
 use Illuminate\Support\ServiceProvider;
 use Shared\Enums\MorphEnum;
@@ -20,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        Model::shouldBeStrict(! $this->app->isProduction());
+
         Relation::morphMap([
             MorphEnum::USER->value => \Domain\Client\Models\User::class,
         ]);
@@ -27,6 +31,7 @@ class AppServiceProvider extends ServiceProvider
         $this->loadMigrationsFrom([
             database_path() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR . 'Client',
         ]);
+
 
         if ($this->app->isLocal()) {
             $this->app->register(\Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class);
